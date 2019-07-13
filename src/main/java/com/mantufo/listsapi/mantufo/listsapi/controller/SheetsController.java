ea5877d7480df3da3a1458cbb9e1921efbbc9a0e
dszendrei <r.daniel.szendrei@gmail.com>
2019-07-13 15:14:09 +0200

    Add crossorigin annotation to getmapping

--- src/main/java/com/mantufo/listsapi/mantufo/listsapi/controller/SheetsController.java
+++ src/main/java/com/mantufo/listsapi/mantufo/listsapi/controller/SheetsController.java
@@ -3,10 +3,7 @@ package com.mantufo.listsapi.mantufo.listsapi.controller;
 import com.mantufo.listsapi.mantufo.listsapi.Model.ConvertedSheet;
 import com.mantufo.listsapi.mantufo.listsapi.service.ConvertedSheetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -17,6 +14,7 @@ public class SheetsController {
     @Autowired
     ConvertedSheetService convertedSheetService;
 
+    @CrossOrigin
     @GetMapping("/{worksheet}/{range}")
     public ConvertedSheet getConvertedSheets(@PathVariable(value = "worksheet", required = false) String worksheet,
                                              @PathVariable(value = "range", required = false) String range) throws IOException {
