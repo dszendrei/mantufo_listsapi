0e0abca3fea556c78a3b37b0460214aa8b1c1d2e
dszendrei <r.daniel.szendrei@gmail.com>
2019-11-12 14:34:27 +0100

    Modify tests

--- src/main/java/com/mantufo/listsapi/mantufo/listsapi/config/SecurityConf.java
+++ src/main/java/com/mantufo/listsapi/mantufo/listsapi/config/SecurityConf.java
@@ -2,9 +2,7 @@ package com.mantufo.listsapi.mantufo.listsapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -15,11 +13,8 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
-import java.util.Collections;
 
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConf extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
--- src/test/java/com/mantufo/listsapi/mantufo/listsapi/service/GoogleSheetsAPIIntegrationTest.java
+++ src/test/java/com/mantufo/listsapi/mantufo/listsapi/service/GoogleSheetsAPIIntegrationTest.java
@@ -24,27 +24,27 @@ public class GoogleSheetsAPIIntegrationTest {
     @Test
     public void testOneCell() throws IOException {
         assertEquals("Egyetem", service
-                .getRange(SPREADSHEET_ID, SheetNames.TANTARGYAK_ITTHON + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.TANTARGYAK_ITTHON) + "A1").getValues().get(0).get(0));
         assertEquals("Ajánlott szakok:", service
-                .getRange(SPREADSHEET_ID, SheetNames.SZAKOK + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.SZAKOK) + "A1").getValues().get(0).get(0));
         assertEquals("Verseny neve", service
-                .getRange(SPREADSHEET_ID, SheetNames.VERSENYEK + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.VERSENYEK) + "A1").getValues().get(0).get(0));
         assertEquals("Diploma", service
-                .getRange(SPREADSHEET_ID, SheetNames.EUROPAI_DIPLOMAK + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.EUROPAI_DIPLOMAK) + "A1").getValues().get(0).get(0));
         assertEquals("Események amikre mutató linket édemes berakni a honlapba:", service
-                .getRange(SPREADSHEET_ID, SheetNames.ESEMENYEK + "A1").getValues().get(0).get(0));
-        assertEquals("Űrkatalógus 2016", service
-                .getRange(SPREADSHEET_ID, SheetNames.KUTATOCSOPORTOK_ES_CEGEK + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.ESEMENYEK) + "A1").getValues().get(0).get(0));
+        assertEquals("Kutatócsoportok és cégek (Űrkatalógus 2016)", service
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.KUTATOCSOPORTOK_ES_CEGEK) + "A1").getValues().get(0).get(0));
         assertEquals("Potenciális partnerek", service
-                .getRange(SPREADSHEET_ID, SheetNames.PARTNEREK + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.PARTNEREK) + "A1").getValues().get(0).get(0));
         assertEquals("Mikor", service
-                .getRange(SPREADSHEET_ID, SheetNames.EDDIGI_MEGJELENESEINK + "A1").getValues().get(0).get(0));
+                .getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.EDDIGI_MEGJELENESEINK) + "A1").getValues().get(0).get(0));
     }
 
     @Test
     public void testHeaders() throws IOException {
         assertArrayEquals(new String[] {"Egyetem", "Tárgy neve", "Tárgy kódja", "Tárgyfelelős neve", "Tanszék", "Link"},
-                service.getRange(SPREADSHEET_ID, "A1:F1").getValues().get(0).toArray());
+                service.getRange(SPREADSHEET_ID, SheetNames.convertSheetNameToGetValues(SheetNames.TANTARGYAK_ITTHON) + "A1:F1").getValues().get(0).toArray());
     }
 
     @Test
